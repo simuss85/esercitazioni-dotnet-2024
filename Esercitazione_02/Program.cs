c5e7945c305bbeda178019eebe07f449d1a0a48c
simuss85 <simone.ussi85@gmail.com>
2024-01-09 12:51:33 +0100

    05 - Dichiarare una variabile di tipo data

--- Esercitazione_02/Program.cs
+++ Esercitazione_02/Program.cs
@@ -2,7 +2,7 @@
 {
     static void Main(string[] args)
     {
-        decimal altezza = 1.80M;
-        Console.WriteLine($"Sei alto {altezza} metri");
+        DateTime dataDiNascita = new DateTime(1985, 7, 30);
+        Console.WriteLine($"Sei nato il {dataDiNascita}");
     } 
 }
