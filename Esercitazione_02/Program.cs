aa33972025512a9e74241d41e6424188e5a6704c
simuss85 <simone.ussi85@gmail.com>
2024-01-11 09:59:01 +0100

    14 - Dichiarare un array di interi

--- Esercitazione_02/Program.cs
+++ Esercitazione_02/Program.cs
@@ -2,9 +2,13 @@
 {
     static void Main(string[] args)
     {
-        int a = 10;
-        int b = 20;
-        bool maggiore = (a > b);
-        System.Console.WriteLine($"Il primo numero è maggiore del secondo? {maggiore}");
+        int[] numeri = new int[3];
+        numeri[0] = 10;
+        numeri[1] = 20;
+        numeri[2] = 20;
+        numeri[3] = "30"; // elemento non di tipo numerico
+        // array che non c'e'
+        System.Console.WriteLine($"I numeri sono {numeri[0]}, {numeri[1]} e {numeri[3]}");
+        System.Console.WriteLine($"I numeri sono {numeri[0]}, {numeri[1]} e {numeri[2]}");
     }
 }
--- Esercitazione_02/README.md
+++ Esercitazione_02/README.md
@@ -214,3 +214,22 @@ class Program
     }
 }
 ```
+
+### 14 - Dichiarare un array di interi:
+
+```c#
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numeri = new int[3];
+        numeri[0] = 10;
+        numeri[1] = 20;
+        numeri[2] = 20;
+        // numeri[3] = "30"; // elemento non di tipo numerico
+        // array che non c'e'
+        // System.Console.WriteLine($"I numeri sono {numeri[0]}, {numeri[1]}, {numeri[2]} e {numeri[3]}");
+        System.Console.WriteLine($"I numeri sono {numeri[0]}, {numeri[1]} e {numeri[2]}");
+    }
+}
+```
