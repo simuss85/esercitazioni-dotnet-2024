6c15a4f6b349710290de34a4d0a827fe702332eb
simuss85 <simone.ussi85@gmail.com>
2024-01-12 09:47:51 +0100

    25 - Utilizzare istruzione if con else-if else

--- Esercitazione_02/Program.cs
+++ Esercitazione_02/Program.cs
@@ -6,12 +6,15 @@
         int b = 20;
         if (a > b) // la condizione da verificare si scrive tra parentesi
         {
-            System.Console.WriteLine($"{a} e' maggiore di {b}");
+            System.Console.WriteLine("{a} e' maggiore di {b}");
         }
         else
         {
             System.Console.WriteLine($"{a} e' minore di {b}");
         }
-        
+        if (a < b)
+        {
+             System.Console.WriteLine("Prova");
+        }
     }
 }
