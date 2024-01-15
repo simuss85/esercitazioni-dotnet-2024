9aab2aa122d6db24a180cd84c78cb9bd02c9cae0
simuss85 <simone.ussi85@gmail.com>
2024-01-15 16:01:49 +0100

    41 - Menu con opzioni

--- Esercitazione_02/Program.cs
+++ Esercitazione_02/Program.cs
@@ -2,20 +2,63 @@
 {
     static void Main(string[] args)
     {
-        System.Console.WriteLine("Trascina un file qui e premi invio:");
-        string? filePath = Console.ReadLine().Trim('"'); // rimuove le virgolette
+        Console.Clear();
+        System.Console.WriteLine("Seleziona l'opzione:");
+        System.Console.WriteLine("1 - Nascondi il cursore");
+        System.Console.WriteLine("2 - Mostra il cursore");
+        System.Console.WriteLine("3 - Pulisci console");
+        System.Console.WriteLine("4 - Emetti beep");
+        System.Console.WriteLine("5 - Emetti beep prolungato");
+        System.Console.WriteLine("6 - Imposta il titolo");
+        System.Console.WriteLine("e - exit\n");
 
-        try
+        while (true)
         {
-            string contenuto = File.ReadAllText(filePath);
-            System.Console.WriteLine("Contenuto del file: ");
-            System.Console.WriteLine(contenuto);
+            System.Console.WriteLine("Digita...");
+            string? input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    Console.CursorVisible = false;
+                    break;
+
+                case "2":
+                    Console.CursorVisible = true;
+                    break;
+
+                case "3":
+                    Console.Clear();
+                    break;
+
+                case "4":
+                    Console.Beep();
+                    break;
+
+                case "5":
+                    System.Console.WriteLine("Inserisci la frequenza");
+                    int freq = Int32.Parse(Console.ReadLine());
+
+                    System.Console.WriteLine("Inserisci durata in ms");
+                    int ms = Int32.Parse(Console.ReadLine());
+
+                    Console.Beep(freq, ms);
+                    break;
+
+                case "6":
+                    Console.Title = "La mia app";
+                    break;
+
+                case "e":
+                    return;
+
+                default:
+                    System.Console.WriteLine("Opzione errata:");
+                    break;
+
+            }
+
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Si è verificato un errore: {ex.Message}");
-            
-        }
-        
+
     }
 }
--- Esercitazione_02/README.md
+++ Esercitazione_02/README.md
@@ -868,7 +868,7 @@ class Program
 
 ```
 
-### 40 - Drag and Drop di un file per leggerne il contenuto
+### 40 - Drag and Drop di un file per leggerne il contenuto:
 
 ```c#
 class Program
@@ -890,6 +890,74 @@ class Program
             
         }
         
+    }
+}
+```
+### 41 - Menu con opzioni:
+
+```c# 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        System.Console.WriteLine("Seleziona l'opzione:");
+        System.Console.WriteLine("1 - Nascondi il cursore");
+        System.Console.WriteLine("2 - Mostra il cursore");
+        System.Console.WriteLine("3 - Pulisci console");
+        System.Console.WriteLine("4 - Emetti beep");
+        System.Console.WriteLine("5 - Emetti beep prolungato");
+        System.Console.WriteLine("6 - Imposta il titolo");
+        System.Console.WriteLine("e - exit\n");
+
+        while (true)
+        {
+            System.Console.WriteLine("Digita...");
+            string? input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    Console.CursorVisible = false;
+                    break;
+
+                case "2":
+                    Console.CursorVisible = true;
+                    break;
+
+                case "3":
+                    Console.Clear();
+                    break;
+
+                case "4":
+                    Console.Beep();
+                    break;
+
+                case "5":
+                    System.Console.WriteLine("Inserisci la frequenza");
+                    int freq = Int32.Parse(Console.ReadLine());
+
+                    System.Console.WriteLine("Inserisci durata in ms");
+                    int ms = Int32.Parse(Console.ReadLine());
+
+                    Console.Beep(freq, ms);
+                    break;
+
+                case "6":
+                    Console.Title = "La mia app";
+                    break;
+
+                case "e":
+                    return;
+
+                default:
+                    System.Console.WriteLine("Opzione errata:");
+                    break;
+
+            }
+
+        }
+
     }
 }
 ```
