30362dd4d80cb0400081e00618d5c8799a4de646
simuss85 <simone.ussi85@gmail.com>
2024-01-18 11:47:52 +0100

    54 - Calcolatrice base con interi, chede prima i numeri

--- Esercitazione_02/Program.cs
+++ Esercitazione_02/Program.cs
@@ -3,8 +3,8 @@
     static void Main(string[] args)
     {
         // semplice calcolatrice con quattro operatori e selettore switch
-        int x, y, risultato;
-        string? input;
+        int x, y, risultato = 0;
+        string? input, outputString = "";
 
         do
         {
@@ -18,130 +18,79 @@
             Console.WriteLine("**  -  sottrazione  **");
             Console.WriteLine("**  /  divisione    **");
             Console.WriteLine("**********************\n");
-            Console.Write("Seleziona opzione: ");
 
+            Console.WriteLine("Inserisci il primo numero");
             input = Console.ReadLine();
 
+            while (!(int.TryParse(input, out x))) // controllo sul primo numero inserito
+            {
+                Console.WriteLine("Devi inserire un numero intero");
+                Console.Write("Inserisci il primo numero: ");
+                input = Console.ReadLine();
+            }
+
+            Console.Write("Inserisci il secondo numero: ");
+            input = Console.ReadLine();
+
+            while (!(int.TryParse(input, out y))) // controllo sul secondo numero inserito
+            {
+                Console.WriteLine("Devi inserire un numero intero");
+                Console.Write("Inserisci il secondo numero: ");
+                input = Console.ReadLine();
+            }
+
+            Console.Write("Seleziona opzione: ");
+            input = Console.ReadLine();
+
+        Verifica:
             switch (input)
             {
                 case "+":       // esegue la somma tra due numeri e visualizza il risultato
-                    Console.WriteLine("Inserisci il primo numero");
-                    input = Console.ReadLine();
-
-                    while (!(int.TryParse(input, out x))) // controllo sul primo numero inserito
-                    {
-                        Console.WriteLine("Devi inserire un numero intero");
-                        Console.Write("Inserisci il primo numero: ");
-                        input = Console.ReadLine();
-                    }
-                    Console.Write("Inserisci il secondo numero: ");
-                    input = Console.ReadLine();
-
-                    while (!(int.TryParse(input, out y))) // controllo sul secondo numero inserito
-                    {
-                        Console.WriteLine("Devi inserire un numero intero");
-                        Console.Write("Inserisci il secondo numero: ");
-                        input = Console.ReadLine();
-                    }
-
                     risultato = x + y;
-                    Console.WriteLine($"La somma tra {x} e {y} = {risultato}");
-                    Console.WriteLine("Premi per continuare...");
-                    Console.ReadKey();
-
-
+                    outputString = "La somma";
                     break;
 
                 case "*":       // esegue la somma tra due numeri e visualizza il risultato
-                    Console.WriteLine("Inserisci il primo numero");
-                    input = Console.ReadLine();
-
-                    while (!(int.TryParse(input, out x))) // controllo sul primo numero inserito
-                    {
-                        Console.WriteLine("Devi inserire un numero intero");
-                        Console.Write("Inserisci il primo numero: ");
-                        input = Console.ReadLine();
-                    }
-                    Console.Write("Inserisci il secondo numero: ");
-                    input = Console.ReadLine();
-
-                    while (!(int.TryParse(input, out y))) // controllo sul secondo numero inserito
-                    {
-                        Console.WriteLine("Devi inserire un numero intero");
-                        Console.Write("Inserisci il secondo numero: ");
-                        input = Console.ReadLine();
-                    }
-
                     risultato = x * y;
-                    Console.WriteLine($"Il prodotto tra {x} e {y} = {risultato}");
-                    Console.WriteLine("Premi per continuare...");
-                    Console.ReadKey();
-
+                    outputString = "Il prodotto";
                     break;
 
                 case "-":       // esegue la somma tra due numeri e visualizza il risultato
-                    Console.WriteLine("Inserisci il primo numero");
-                    input = Console.ReadLine();
-
-                    while (!(int.TryParse(input, out x))) // controllo sul primo numero inserito
-                    {
-                        Console.WriteLine("Devi inserire un numero intero");
-                        Console.Write("Inserisci il primo numero: ");
-                        input = Console.ReadLine();
-                    }
-                    Console.Write("Inserisci il secondo numero: ");
-                    input = Console.ReadLine();
-
-                    while (!(int.TryParse(input, out y))) // controllo sul secondo numero inserito
-                    {
-                        Console.WriteLine("Devi inserire un numero intero");
-                        Console.Write("Inserisci il secondo numero: ");
-                        input = Console.ReadLine();
-                    }
-
                     risultato = x - y;
-                    Console.WriteLine($"La sottrazione tra {x} e {y} = {risultato}");
-                    Console.WriteLine("Premi per continuare...");
-                    Console.ReadKey();
-
+                    outputString = "La sottrazione";
                     break;
 
                 case "/":       // esegue la somma tra due numeri e visualizza il risultato
-                    Console.WriteLine("Inserisci il primo numero");
-                    input = Console.ReadLine();
-
-                    while (!(int.TryParse(input, out x))) // controllo sul primo numero inserito
+                    while (y == 0)
                     {
-                        Console.WriteLine("Devi inserire un numero intero");
-                        Console.Write("Inserisci il primo numero: ");
-                        input = Console.ReadLine();
-                    }
-                    Console.Write("Inserisci il secondo numero: ");
-                    input = Console.ReadLine();
+                        Console.WriteLine("Non puoi dividere per ZERO!");
+                        while (!(int.TryParse(input, out y)) || (int.Parse(input) == 0)) // controllo sul secondo numero inserito
+                        {
+                            Console.WriteLine("Devi inserire un numero intero");
+                            Console.Write("Inserisci il secondo numero: ");
+                            input = Console.ReadLine();
+                        }
 
-                    while (!(int.TryParse(input, out y)) || (int.Parse(input) == 0)) // controllo sul secondo numero inserito
-                    {
-                        Console.WriteLine("\nATTENZIONE!!!\nDevi inserire un numero intero e diverso da ZERO\n");
-                        Console.Write("Inserisci il secondo numero: ");
-                        input = Console.ReadLine();
                     }
-
                     risultato = x / y;
                     int resto = x % y;
-                    Console.WriteLine($"La divisione intera tra {x} e {y} = {risultato} con resto {resto}");
-                    Console.WriteLine("Premi per continuare...");
-                    Console.ReadKey();
-
+                    outputString = "La divisione";
                     break;
 
                 default:
                     Console.WriteLine("Selezione errata.");
-                    Console.WriteLine("Premi per continuare...");
-                    Console.ReadKey();
-                    break;
+                    Console.Write("Seleziona opzione: ");
+                    input = Console.ReadLine();
+                    goto Verifica;
 
             }
-
+            Console.WriteLine($"{outputString} tra {x} e {y} = {risultato}");
+            Console.WriteLine("Vuoi continuare? s/n");
+            input = Console.ReadLine();
+            if (input == "n")
+            {
+                return;
+            }
         }
         while (true);
     }
--- Esercitazione_02/README.md
+++ Esercitazione_02/README.md
@@ -1527,7 +1527,7 @@ class Program
     }
 ```
 
-### 53 - Calcolatrice base con interi:
+### 53 - Calcolatrice base con interi, chiede prima l'opzione:
 
 ```c#
 class Program
@@ -1550,6 +1550,7 @@ class Program
             Console.WriteLine("**  -  sottrazione  **");
             Console.WriteLine("**  /  divisione    **");
             Console.WriteLine("**********************\n");
+
             Console.Write("Seleziona opzione: ");
 
             input = Console.ReadLine();
@@ -1674,6 +1675,112 @@ class Program
 
             }
 
+        }
+        while (true);
+    }
+
+}
+```
+
+### 54 - Calcolatrice base con interi, chede prima i numeri:
+
+```c#
+using System.Diagnostics.Tracing;
+using System.Security;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // semplice calcolatrice con quattro operatori e selettore switch
+        int x, y, risultato = 0;
+        string? input, outputString = "";
+
+        do
+        {
+            Console.Clear();
+
+            Console.WriteLine("**********************");
+            Console.WriteLine("**** CALCOLATRICE ****");
+            Console.WriteLine("**********************");
+            Console.WriteLine("**  +  somma        **");
+            Console.WriteLine("**  *  prodotto     **");
+            Console.WriteLine("**  -  sottrazione  **");
+            Console.WriteLine("**  /  divisione    **");
+            Console.WriteLine("**********************\n");
+
+            Console.WriteLine("Inserisci il primo numero");
+            input = Console.ReadLine();
+
+            while (!(int.TryParse(input, out x))) // controllo sul primo numero inserito
+            {
+                Console.WriteLine("Devi inserire un numero intero");
+                Console.Write("Inserisci il primo numero: ");
+                input = Console.ReadLine();
+            }
+
+            Console.Write("Inserisci il secondo numero: ");
+            input = Console.ReadLine();
+
+            while (!(int.TryParse(input, out y))) // controllo sul secondo numero inserito
+            {
+                Console.WriteLine("Devi inserire un numero intero");
+                Console.Write("Inserisci il secondo numero: ");
+                input = Console.ReadLine();
+            }
+
+            Console.Write("Seleziona opzione: ");
+            input = Console.ReadLine();
+
+        Verifica:
+            switch (input)
+            {
+                case "+":       // esegue la somma tra due numeri e visualizza il risultato
+                    risultato = x + y;
+                    outputString = "La somma";
+                    break;
+
+                case "*":       // esegue la somma tra due numeri e visualizza il risultato
+                    risultato = x * y;
+                    outputString = "Il prodotto";
+                    break;
+
+                case "-":       // esegue la somma tra due numeri e visualizza il risultato
+                    risultato = x - y;
+                    outputString = "La sottrazione";
+                    break;
+
+                case "/":       // esegue la somma tra due numeri e visualizza il risultato
+                    while (y == 0)
+                    {
+                        Console.WriteLine("Non puoi dividere per ZERO!");
+                        while (!(int.TryParse(input, out y)) || (int.Parse(input) == 0)) // controllo sul secondo numero inserito
+                        {
+                            Console.WriteLine("Devi inserire un numero intero");
+                            Console.Write("Inserisci il secondo numero: ");
+                            input = Console.ReadLine();
+                        }
+
+                    }
+                    risultato = x / y;
+                    int resto = x % y;
+                    outputString = "La divisione";
+                    break;
+
+                default:
+                    Console.WriteLine("Selezione errata.");
+                    Console.Write("Seleziona opzione: ");
+                    input = Console.ReadLine();
+                    goto Verifica;
+
+            }
+            Console.WriteLine($"{outputString} tra {x} e {y} = {risultato}");
+            Console.WriteLine("Vuoi continuare? s/n");
+            input = Console.ReadLine();
+            if (input == "n")
+            {
+                return;
+            }
         }
         while (true);
     }
