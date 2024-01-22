5fd02e55f2d292ad8605f021de18f8a3d156911e
Simone <Simo@MacBook-Pro-di-Emanuela.local>
2024-01-22 12:09:30 +0100

    61 - Indovina il numero segreto 'THE GAME'

---
+++ Esercitazione_01/Test.cs
@@ -0,0 +1,31 @@
+﻿enum Color
+{
+    Red = 15,
+    Green = 25,
+    Blue = 35
+}
+
+class Test
+{
+    static void Main()
+    {
+        Console.WriteLine(StringFromColor(Color.Red));
+        Console.WriteLine(StringFromColor(Color.Green));
+        Console.WriteLine(StringFromColor(Color.Blue));
+    }
+
+    static string StringFromColor(Color c)
+    {
+        switch (c)
+        {
+            case Color.Red:
+                return $"Red = {(int)c}";
+            case Color.Green:
+                return $"Green = {(int)c}";
+            case Color.Blue:
+                return $"Blue = {(int)c}";
+            default:
+                return "Invalid color";
+        }
+    }
+}
