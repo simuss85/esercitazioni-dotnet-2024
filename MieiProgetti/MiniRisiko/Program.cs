85639bb29c29c20aa0d0f2c657f6cdb2a5ded518
simuss85 <simone.ussi85@gmail.com>
2024-01-29 16:01:46 +0100

    Aggiunge alcuni metodi di base per il programma
    Inseriti tag region per suddividere il codice
    Create le funzioni per i menu di selezione

---
+++ MieiProgetti/MiniRisiko/Program.cs
@@ -0,0 +1,180 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        #region "Tipi di dati e variabili"
+        string benvenuto = "Benvenuto a MiniRisiko";
+        string? input;
+        string pathSave = @"save.txt";
+        string pathPlayer = @"map.txt";
+        string[] player1 = ["Nome", "green"];
+        string[] palyer2 = ["PC", "Colore"]; //in caso di gioco vs altro utente il nome verrà sovrascritto
+        bool giocoInCorso = true;
+
+        #endregion
+
+        Console.Clear();
+        //messaggio di benvenuto
+        Console.WriteLine($"{benvenuto}\n");
+        ColoreUtente(player1);
+        FingiDiPensare(5, 600);
+
+
+        //visualizzo il menù iniziale del gioco
+        CreaMenuSelezionePrincipale();
+
+        do
+        {
+            input = Console.ReadLine();
+
+
+            switch (input)
+            {
+                case "1":
+
+                    break;
+
+                case "2":
+
+                    break;
+
+                case "3":
+
+                    break;
+
+                case "4":
+
+                    break;
+
+                case "5":
+
+                    break;
+
+                default:
+
+                    break;
+            }
+
+        }
+        while (giocoInCorso);
+
+
+    }
+
+    #region "Metodi interfaccia l'utente"
+    static void CreaMenuSelezionePrincipale()
+    {
+        Console.WriteLine("Seleziona l'opzione:\n");
+        Console.WriteLine("1. Gioca contro il pc");
+        Console.WriteLine("2. Gioca contro altro utente");
+        Console.WriteLine("3. Carica ultima partita");
+        Console.WriteLine("4. Regole del gioco");
+        Console.WriteLine("5. Esci");
+    }
+
+    static void MenuSelezioneColoreUtente()
+    {
+        Console.WriteLine("r. Rosso");
+        Console.WriteLine("b. Blu");
+        Console.WriteLine("n. Nero");
+        Console.WriteLine("g. Giallo");
+        Console.WriteLine("v. Verde");
+    }
+
+    static void MenuDiGioco()
+    {
+        Console.WriteLine("1. Lancia i dadi");
+        Console.WriteLine("2. Modalità rischio");
+        Console.WriteLine("s. Salva il gioco ed esci");
+        Console.WriteLine("'ctrl'+'M' Visualizza mappa");
+    }
+
+    static void DisegnaMappa()
+    {
+        Console.WriteLine("           Mappa del mondo");
+        Console.WriteLine("   |AMERICA|EUROPA|ASIA|POLO NORD|");
+        Console.WriteLine("|SUD AMERICA|AFRICA|OCEANIA|POLO SUD|");
+
+    }
+
+    static void MenuModalitaRischio()
+    {
+        Console.WriteLine("1. Scommetti su pari o dispari (costo 1 continente)");
+        Console.WriteLine("2. Scommetti sul numero esatto (costo 2 continenti)");
+        Console.WriteLine("r. Ritorna alla schermata precedente");
+    }
+    #endregion
+
+    #region "Metodi accessori"
+
+    /*Simula un caricamento del programma mediante l'utilizzo del separatore '.'.
+      Stampa i puntini in base al numero inserito in input e ritorna con il cursore
+      sulla stessa linea.
+      Si consiglia di inserire un tempo in ms entro i 1000.
+
+      Input: int punti --> numero di puntini da visualizzare a schermo
+             int ms -----> tempo di attesa in ms per la funzione Sleep
+    */
+    static void FingiDiPensare(int punti, int ms)
+    {
+        for (int i = 0; i < punti; i++)
+        {
+            Console.Write(".");
+            Thread.Sleep(ms);
+        }
+        Console.Write("\r");
+    }
+
+    /*Cambia il colore del carattere in base all'utente attuale, prendere i dati dall'array 
+      player. Scrive il nome del colore selezionato e resetta il colore di default
+    
+      Input: string[] player ---> array di due elementi ["nome","colore"]
+
+    */
+    static void ColoreUtente(string[] player)
+    {   
+        var colore = ConsoleColor.Red;
+        switch (player[1])
+        {
+            case "green":
+                colore = ConsoleColor.Green;
+                break;
+
+        }
+        Console.ForegroundColor = colore;
+        Console.WriteLine($"{player[1]}");
+        Console.ResetColor();
+
+    }
+
+    #endregion
+
+    #region "Metodi di gioco"
+
+    static void GiocaVsPc()
+    {
+
+    }
+
+    static void GiocaVsUtente()
+    {
+
+    }
+
+    static void CaricaPartita()
+    {
+
+    }
+
+    static void SalvaPartita()
+    {
+
+    }
+
+    static void RegoleGioco()
+    {
+
+    }
+    #endregion
+
+}
---
+++ MieiProgetti/MiniRisiko/README.md
@@ -0,0 +1,72 @@
+# PROGETTO GIOCO DI DADI
+
+### Descrizone del gioco:
+Gioco *stile Risiko* versione super semplificata, con la mappa costituita dai soli 7 continenti (piu i due poli) senza la suddivisione delle varie nazioni. L'obiettivo finale è quello di conquistare tutti i continenti utilizzando le modalita seguenti: 
+>**Lanciando i dadi**: ottenendo il punteggio maggiore contro l'avversario.  
+
+Il gioco prevede anche una *modalità rischio*, attivabile solo 1 volta per giocatore, con le due modalità seguenti:  
+
+>**Pari o dispari:** viene lanciato un dado a 36 facce. Ogni utente mette in palio 1 continente in suo possesso, in caso di vittoria se ne ottengono 2 (1 in palio e 1 a scelta). A turno ogni giocatore chiede all'altro la predizione e poi lancia il dado. Il primo che sbaglia nel corso di un turno, perde.  
+
+>**Numero esatto:** viene lanciato un dado a 50 facce. Ogni utente mette in palio 2 continenti in suo possesso, in caso di vittoria se ne ottengono 3 (2 in palio e 1 a scelta). A turno viene chiesto ad un giocatore la predizione del numero e poi viene lanciato il dado. Chi si avvicina di più al numero segreto vince. In caso di parità si ripete fino al decretamento del vincitore.
+Nel caso uno dei due utenti non sia in possesso di 2 continenti, l'opzione non è selezionabile.
+
+
+
+
+### Specifiche generali:
+
+- [x] L'applicazione deve permettere di insiere il nome utente e fargli scegliere un colore per le sue armate.
+- [x] L'applicazione deve creare di volta in volta la mappa dei continenti di tipo testuale, aggiornata ad ogni turno di gioco.  
+- [x] Il gioco prevede l'utilizzo di due dadi a 6 facce per turno.
+- [x] Una volta per gioco un utente può decidere se scommettere anche su "Pari o dispari" oppure "Numero esatto".
+- [x] Possibilità di giocare sia contro il pc che contro un altro utente umano.  
+- [x] Possibilità di salvare la partita e riprenderla successivamente utilizzando file.txt.
+- [x] Possibilità di visualizzare ad ogni turno la situazione attuale della mappa mediante una combinazione di tasti "ctrl"+"m".
+
+
+
+
+### Interazione con l'utente e Design dell'Architettura:
+- [x] Menu di selezione iniziale con 5 opzioni:   
+    >1.gioca contro pc,  
+    >2.gioca contro giocatore 2  
+    >3.riprendi ultima partita  
+    >4.Regole di gioco  
+    >5.exit. 
+
+- [x] Menu di selezione colore:
+    >r.Rosso  
+    >b.Blu  
+    >n.Nero  
+    >g.Giallo  
+    >v.Verde  
+
+- [x] Menu di gioco giocatore con 3 opzioni:  
+    >1.lancia i dadi  
+    >2.rischia  
+    >s. salva il gioco  
+    >(ctrl+M visualizza mappa)
+
+- [x] Menu modalità rischio:  
+    >1.Scommetti su pari o dispari (costo 1 continente)  
+    >2.Scommetti sul numero esatto (costo 2 continenti)  
+    >r.Ritorna alla schermata precedente
+
+- [x] Menu pari o dispari:
+    >1.Pari  
+    >2.Dispari
+
+- [x] Menu numero esatto: 
+    >inserisci il numero
+
+- [x] Mappa dei continenti:  
+    >   MAPPA DEL MONDO  
+    >|AMERICA|ASIA|EUROPA|SUD AMERICA|AFRICA|OCEANIA|POLO SUD|POLO NORD|
+
+- [x] Richiesta input per l'inserimento nome/i
+- [x] Richiesta input per l'inserimento del numero esatto (da 1 a 50)
+
+
+
+### Test e Debugging
