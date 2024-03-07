633df0c6697a8d823f18bd149deedb738f736b3f
simuss85 <simone.ussi85@gmail.com>
2024-03-07 12:37:14 +0100

    Aggiunge comando di avvio app

--- MieiProgetti/FoodexpMvc/Program.cs
+++ MieiProgetti/FoodexpMvc/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FoodexpMvc.Controllers;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Controller.AvvioApp();
+    }
+}
