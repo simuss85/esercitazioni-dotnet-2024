3d661fc54a50ef77f5e1a0bdc806425b9c3697dd
simuss85 <simone.ussi85@gmail.com>
2024-03-11 11:09:23 +0100

    Inserisce tutti i metodi Read

--- MieiProgetti/FoodexpMvc/Controllers/AlimentiController.cs
+++ MieiProgetti/FoodexpMvc/Controllers/AlimentiController.cs
@@ -1,5 +1,6 @@
 using FoodexpMvc.Models;
 using FoodexpMvc.Views;
+using Microsoft.EntityFrameworkCore;
 
 namespace FoodexpMvc.Controllers
 {
@@ -14,6 +15,7 @@ namespace FoodexpMvc.Controllers
         public static void SelezioneMenu()
         {
             //frigorifero - filtra alimenti
+            bool inverti;
             eseguito = false;
             while (!eseguito)
             {
@@ -36,30 +38,154 @@ namespace FoodexpMvc.Controllers
 
                     case "3":
                         //visualizza in scadenza
+                        int giorni = 1;
+                        AlimentiView.VisualizzaAlimenti(GetAlimentiScaduti(giorni), $"Scaduti o in scadenza ({giorni} giorno/i)\n");
                         break;
 
                     case "4":
                         //visualizza in esaurimento
+                        int quantita = 1;
+                        AlimentiView.VisualizzaAlimenti(GetAlimentiEsaurimento(quantita), $"In esaurimento (quantità {quantita})\n");
                         break;
 
                     case "5":
                         //ordina per nome
+                        inverti = false;
+                        do
+                        {
+                            AlimentiView.VisualizzaAlimenti(GetAlimentiPerNome(), $"Ordine per nome\n");
+                            Console.WriteLine("\n...vuoi invertire l'ordine della lista? (s/n)");
+                            input = Console.ReadLine()!.ToLower();
+                            if (input == "s")
+                            {
+                                inverti = true;
+                                Console.Clear();
+
+                                AlimentiView.VisualizzaAlimenti(GetAlimentiPerNome(inverti), $"Ordine per nome decrescente\n");
+                            }
+                            else if (input == "n")
+                            {
+                                inverti = true; //condizione di uscita dal while senza forzarlo
+                            }
+                            else
+                            {
+                                View.MessaggioSelezioneErrata();
+                                Console.Clear();
+                            }
+                        }
+                        while (!inverti);
                         break;
 
                     case "6":
                         //ordina per data di scadenza
+                        inverti = false;
+                        do
+                        {
+                            AlimentiView.VisualizzaAlimenti(GetAlimentiPerScadenza(), $"Ordine per data di scadenza\n");
+                            Console.WriteLine("\n...vuoi invertire l'ordine della lista? (s/n)");
+                            input = Console.ReadLine()!.ToLower();
+                            if (input == "s")
+                            {
+                                inverti = true;
+                                Console.Clear();
+
+                                AlimentiView.VisualizzaAlimenti(GetAlimentiPerScadenza(inverti), $"Ordine per data di scadenza decrescente\n");
+                            }
+                            else if (input == "n")
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                View.MessaggioSelezioneErrata(1000);
+                                Console.Clear();
+                            }
+                        }
+                        while (!inverti);
                         break;
 
                     case "7":
                         //ordina per data di inserimento
+                        inverti = false;
+                        do
+                        {
+                            AlimentiView.VisualizzaAlimenti(GetAlimentiPerInserimento(), $"Ordine per data di inserimento\n");
+                            Console.WriteLine("\n...vuoi invertire l'ordine della lista? (s/n)");
+                            input = Console.ReadLine()!.ToLower();
+                            if (input == "s")
+                            {
+                                inverti = true;
+                                Console.Clear();
+
+                                AlimentiView.VisualizzaAlimenti(GetAlimentiPerInserimento(inverti), $"Ordine per data di inserimento decrescente\n");
+                            }
+                            else if (input == "n")
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                View.MessaggioSelezioneErrata(1000);
+                                Console.Clear();
+                            }
+                        }
+                        while (!inverti);
                         break;
 
                     case "8":
                         //ordina per quantità
+                        inverti = false;
+                        do
+                        {
+                            AlimentiView.VisualizzaAlimenti(GetAlimentiPerQuantita(), $"Ordine per quantità\n");
+                            Console.WriteLine("\n...vuoi invertire l'ordine della lista? (s/n)");
+                            input = Console.ReadLine()!.ToLower();
+                            if (input == "s")
+                            {
+                                inverti = true;
+                                Console.Clear();
+
+                                AlimentiView.VisualizzaAlimenti(GetAlimentiPerQuantita(inverti), $"Ordine per quantità decrescente\n");
+                            }
+                            else if (input == "n")
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                View.MessaggioSelezioneErrata(1000);
+                                Console.Clear();
+                            }
+                        }
+                        while (!inverti);
                         break;
 
                     case "9":
                         //ordina per categoria
+                        inverti = false;
+                        do
+                        {
+                            AlimentiView.VisualizzaAlimenti(GetAlimentiPerCategoria(), $"Ordine per categoria alimenti\n");
+                            Console.WriteLine("\n...vuoi invertire l'ordine della lista? (s/n)");
+                            input = Console.ReadLine()!.ToLower();
+                            if (input == "s")
+                            {
+                                inverti = true;
+                                Console.Clear();
+
+                                AlimentiView.VisualizzaAlimenti(GetAlimentiPerCategoria(inverti), $"Ordine per categoria decrescente\n");
+                            }
+                            else if (input == "n")
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                View.MessaggioSelezioneErrata(1000);
+                                Console.Clear();
+                            }
+                        }
+                        while (!inverti);
                         break;
 
                     case "r":
@@ -73,7 +199,7 @@ namespace FoodexpMvc.Controllers
                         View.MessaggioSelezioneErrata();
                         break;
                 }
-                Console.WriteLine("\n...premi un tasto");
+                Console.Write("\n...premi un tasto");
                 Console.ReadKey();
                 Console.Clear();
 
@@ -127,49 +253,198 @@ namespace FoodexpMvc.Controllers
         #region R - Leggi tabella alimenti
         /// <summary>
         /// Seleziona tutti gli alimenti presenti in db.Alimenti
-        /// <b>**da modificare**</b>
         /// </summary>
         /// <returns>La lista degli alimenti da stampare</returns>
         public static List<string> GetAlimenti()
         {
-            int conta = 1;
-
             List<string> lista = new();
             foreach (var a in _db.Alimenti)
             {
-                lista.Add($"{conta} - {a.Nome} {a.Quantita} {a.DataScadenza}");
-                conta++;
+                lista.Add($"{a.Nome} {a.Quantita} {a.DataScadenza!.Value:d}"); //per la data ottengo il valore e formatto output con solo la data
             }
             return lista;
         }
 
         /// <summary>
         /// Filtra gli alimenti per data di scadenza (minore o uguale alla data odierna), oppure <br/>
-        /// per alimenti che stanno per scadere (scadenza = 1)
+        /// per alimenti che stanno per scadere (range = 0 default).
         /// </summary>
-        /// <param name="scadenza">Valore da sommare alla data attuale</param>
-        /// <returns></returns>
-        public static List<string> GetAlimentiScaduti(int scadenza = 0)
+        /// <param name="range">Valore da sommare alla data attuale</param>
+        /// <returns>La lista degli alimenti da stampare</returns>
+        public static List<string> GetAlimentiScaduti(int range = 0)
         {
-            int conta = 1;
-
             List<string> lista = new();
             foreach (var a in _db.Alimenti)
             {
-                if (scadenza != 0)
+                if (range == 0)      //verifica scadenza esatta
                 {
-                    if (a.DataScadenza == Date.Oggi())
+                    if (a.DataScadenza <= DateTime.Today)
                     {
-                        lista.Add($"{conta} - {a.Nome} {a.Quantita} {a.DataScadenza}");
-                        conta++;
+                        lista.Add($"{a.Nome} {a.Quantita} {a.DataScadenza.Value:d}");
                     }
                 }
-                else
+                else                //verifica scadenza prossima di giorni
                 {
-                    //TO DO
+                    if (a.DataScadenza <= DateTime.Today.AddDays(range))
+                    {
+                        lista.Add($"{a.Nome} {a.Quantita} {a.DataScadenza.Value:d}");
+                    }
                 }
 
             }
+            return lista;
+        }
+
+        /// <summary>
+        /// Filtra gli alimenti per quantità in esaurimento (range = 1 default).
+        /// </summary>
+        /// <param name="range">Valore di controllo per la quantità da considerare</param>
+        /// <returns>La lista degli alimenti da stampare</returns>
+        public static List<string> GetAlimentiEsaurimento(int range = 1)
+        {
+            List<string> lista = new();
+            foreach (var a in _db.Alimenti)
+            {
+                if (a.Quantita <= range)
+                {
+                    lista.Add($"{a.Nome} {a.Quantita} {a.DataScadenza!.Value:d}");
+                }
+            }
+            return lista;
+        }
+
+        /// <summary>
+        /// Filtra gli alimenti per nome e li ordina in ordine crescente (default) <br/>
+        /// oppre in ordine decrescente (valore true).
+        /// </summary>
+        /// <param name="inverti">Se impostato a <b>true</b> ordina la lista decrescente</param>
+        /// <returns>La lista degli alimenti da stampare</returns>
+        public static List<string> GetAlimentiPerNome(bool inverti = false)
+        {
+            List<string> lista = new();
+            foreach (var a in _db.Alimenti)
+            {
+                lista.Add($"{a.Nome} {a.Quantita} {a.DataScadenza!.Value:d}");
+            }
+            //riordino la lista
+            lista.Sort();
+
+            if (inverti) //nel caso la volessi in ordine decrescente
+            {
+                lista.Reverse();    //inverto l'ordine degli elementi
+            }
+            return lista;
+        }
+
+        /// <summary>
+        /// Filtra gli alimenti per data di scadenza in ordine decrecente (default) <br/>
+        /// oppure in  ordine decrecente (valore true).
+        /// </summary>
+        /// <param name="inverti">Se impostato a <b>true</b> ordina la lista decrescente</param>
+        /// <returns>La lista degli alimenti da stampare</returns>
+        public static List<string> GetAlimentiPerScadenza(bool inverti = false)
+        {
+            List<string> lista = new();
+            List<Alimento> alimenti = new();
+            //utilizzo il metodo OrderBy per avere elementi gia ordinati
+            if (inverti)
+            {
+                alimenti = _db.Alimenti.OrderByDescending(a => a.DataScadenza).ToList();
+            }
+            else
+            {
+                alimenti = _db.Alimenti.OrderBy(a => a.DataScadenza).ToList();
+            }
+
+            foreach (var a in alimenti)
+            {
+                lista.Add($"{a.Nome} {a.Quantita} {a.DataScadenza!.Value:d}");
+            }
+
+            return lista;
+        }
+
+        /// <summary>
+        /// Filtra gli alimenti per data di inserimento in ordine decrecente (default) <br/>
+        /// oppure in  ordine decrecente (valore true).
+        /// </summary>
+        /// <param name="inverti">Se impostato a <b>true</b> ordina la lista decrescente</param>
+        /// <returns>La lista degli alimenti da stampare</returns>
+        public static List<string> GetAlimentiPerInserimento(bool inverti = false)
+        {
+            List<string> lista = new();
+            List<Alimento> alimenti = new();
+            //utilizzo il metodo OrderBy per avere elementi gia ordinati
+            if (inverti)
+            {
+                alimenti = _db.Alimenti.OrderByDescending(a => a.DataInserimento).ToList();
+            }
+            else
+            {
+                alimenti = _db.Alimenti.OrderBy(a => a.DataInserimento).ToList();
+            }
+
+            foreach (var a in alimenti)
+            {
+                lista.Add($"{a.Nome} {a.Quantita} {a.DataScadenza!.Value:d} {a.DataInserimento!.Value:d}");
+            }
+
+            return lista;
+        }
+
+        /// <summary>
+        /// Filtra gli alimenti per quantita in ordine decrecente (default) <br/>
+        /// oppure in  ordine decrecente (valore true).
+        /// </summary>
+        /// <param name="inverti">Se impostato a <b>true</b> ordina la lista decrescente</param>
+        /// <returns>La lista degli alimenti da stampare</returns>
+        public static List<string> GetAlimentiPerQuantita(bool inverti = false)
+        {
+            List<string> lista = new();
+            List<Alimento> alimenti = new();
+            //utilizzo il metodo OrderBy per avere elementi gia ordinati
+            if (inverti)
+            {
+                alimenti = _db.Alimenti.OrderByDescending(a => a.Quantita).ToList();
+            }
+            else
+            {
+                alimenti = _db.Alimenti.OrderBy(a => a.Quantita).ToList();
+            }
+
+            foreach (var a in alimenti)
+            {
+                lista.Add($"{a.Nome} {a.Quantita} {a.DataScadenza!.Value:d}");
+            }
+
+            return lista;
+        }
+
+        /// <summary>
+        /// Filtra gli alimenti per quantita in ordine decrecente (default) <br/>
+        /// oppure in  ordine decrecente (valore true).
+        /// </summary>
+        /// <param name="inverti">Se impostato a <b>true</b> ordina la lista decrescente</param>
+        /// <returns>La lista degli alimenti da stampare</returns>
+        public static List<string> GetAlimentiPerCategoria(bool inverti = false)
+        {
+            List<string> lista = new();
+            List<Alimento> alimenti = new();
+            //utilizzo il metodo OrderBy per avere elementi gia ordinati
+            if (inverti)
+            {
+                alimenti = _db.Alimenti.Include(c => c.Categoria).OrderByDescending(c => c.Categoria!.Nome).ToList();
+            }
+            else
+            {
+                alimenti = _db.Alimenti.Include(c => c.Categoria).OrderBy(c => c.Categoria!.Nome).ToList();
+            }
+
+            foreach (var a in alimenti)
+            {
+                lista.Add($"{a.Nome} {a.Quantita} {a.DataScadenza!.Value:d} {a.Categoria!.Nome}");
+            }
+
             return lista;
         }
         #endregion
