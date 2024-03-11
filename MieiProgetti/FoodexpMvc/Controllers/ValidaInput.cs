186d8871c6a94f307804c2bfb112f3bf3d5b5ee2
simuss85 <simone.ussi85@gmail.com>
2024-03-11 15:53:38 +0100

    Crea classe ValidazioneInput

---
+++ MieiProgetti/FoodexpMvc/Controllers/ValidaInput.cs
@@ -0,0 +1,132 @@
+using FoodexpMvc.Views;
+
+namespace FoodexpMvc.Controllers;
+public class ValidaInput
+{
+    // public static string GetPassword()
+    // {
+
+    // }
+
+    public static string GetString(int left, int top)
+    {
+        string errore = "Devi inserire un nome valido\r";
+        string input = Console.ReadLine()!;
+        while (input == "" || int.TryParse(input, out int x))
+        {
+            Console.Write(errore);
+            View.PulisciRiga(errore.Length, left, top);
+            input = Console.ReadLine()!;
+        }
+        return input;
+    }
+
+    public static DateTime GetData(int left, int top)
+    {
+        bool corretto = false;
+        string input;
+        string erroreScaduto = "alimento gia scaduto\r";
+        string erroreFormato = "formato data errato\r";
+        string[] data = new string[3];
+        DateTime oggettoData = new();
+        var annoInCorso = DateTime.Today.Year;    //memorizzo l' anno in corso
+        var meseInCorso = DateTime.Today.Month;
+        var oggi = DateTime.Today.Day;
+
+        while (!corretto)
+        {
+            input = Console.ReadLine()!;
+
+            try
+            {
+                data = input.Split("/");
+                //converto in valore numerico per facilitare i controlli
+                int anno = int.Parse(data[2]);
+                int mese = int.Parse(data[1]);
+                int giorno = int.Parse(data[0]);
+                // Console.WriteLine("anno: " + anno); //DEBUG
+                // Console.WriteLine("mese: " + mese); //DEBUG
+                // Console.WriteLine("giorno: " + giorno); //DEBUG
+                // Console.ReadKey();                      //DEBUG
+
+
+                if (anno < annoInCorso) //anno precedente quindi scaduto
+                {
+                    if (anno < 2000)
+                    {
+                        throw new Exception();
+                    }
+
+                    Console.Write(erroreScaduto);
+                    View.PulisciRiga(erroreScaduto.Length, left, top);
+                    continue;
+                }
+                else if (anno >= annoInCorso)      //anno attuale verifico mese e giorno
+                {
+                    if (mese > 13 || mese < 1)  //mese errato
+                    {
+                        throw new Exception();
+                    }
+                    if (giorno > 31 || giorno < 1)  //giorno errato
+                    {
+                        throw new Exception();
+                    }
+
+                    if (anno == annoInCorso && mese < meseInCorso) //anno in  corso ma scaduto
+                    {
+                        Console.Write(erroreScaduto);
+                        View.PulisciRiga(erroreScaduto.Length, left, top);
+                        continue;
+                    }
+                    else if (anno == annoInCorso && mese == meseInCorso)   //anno in corso e mese in corso
+                    {
+                        if (giorno < oggi)  //scaduto
+                        {
+                            Console.Write(erroreScaduto);
+                            View.PulisciRiga(erroreScaduto.Length, left, top);
+                            continue;
+                        }
+                    }
+                }
+
+                //inserisco i valori per la data
+                oggettoData = new DateTime(anno, mese, giorno);
+
+
+            }
+            catch (Exception)
+            {
+                Console.Write(erroreFormato);
+                View.PulisciRiga(erroreScaduto.Length, left, top);
+                continue;
+            }
+            corretto = true;
+        }
+        return oggettoData;
+    }
+
+    public static int GetInt(int left, int top)
+    {
+        string errore = "Devi inserire una quantita valida\r";
+        int x = 0;
+        while (!int.TryParse(Console.ReadLine()!, out x))
+        {
+            Console.WriteLine(errore);
+            View.PulisciRiga(errore.Length, left, top);
+        }
+        return x;
+    }
+
+    public static int GetIntCategoria(int totaleCategorie, int left, int top)
+    {
+        string errore = "Devi inserire una quantita valida\r";
+        int x = 0;
+        //se non inserisco in numero oppure se supero il range delle categorie
+        while (!int.TryParse(Console.ReadLine()!, out x) || (x < 1 && x > totaleCategorie))
+        {
+            Console.WriteLine(errore);
+            View.PulisciRiga(errore.Length, left, top);
+        }
+        return x;
+    }
+}
