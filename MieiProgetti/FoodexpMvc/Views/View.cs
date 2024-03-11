ca5fed54e8a18c7247d0b6153948945fc640751c
simuss85 <simone.ussi85@gmail.com>
2024-03-11 14:08:35 +0100

    Aggiunge metodo PulisciConsole

--- MieiProgetti/FoodexpMvc/Views/View.cs
+++ MieiProgetti/FoodexpMvc/Views/View.cs
@@ -39,13 +39,52 @@ namespace FoodexpMvc.Views
         }
 
         /// <summary>
-        /// Messaggio "...selezione errata" con attesa (default ms = 600).
+        /// Messaggio "...torno al menu precedente" con attesa (default ms = 600).
         /// </summary>
         /// <param name="ms">Tempo di attesa per il Thread.Sleep (600 ms default)</param>
-        public static void MessaggioSelezioneErrata(int ms = 600)
+        public static void MessaggioTornaMenuPrecedente(int ms = 600)
         {
+            Console.WriteLine("\n...torno al menu precedente"); //TO DO spectre
+            Thread.Sleep(ms);
+        }
+
+        /// <summary>
+        /// Messaggio "...selezione errata" con attesa (default ms = 600). <br/>
+        /// Puo anche ripristinare il cursore a fine messaggio cancellandolo.
+        /// </summary>
+        /// <param name="ms">Tempo di attesa per il Thread.Sleep (600 ms default)</param>
+        /// <param name="resetCursore">Se true cancella il messaggio e ripristina il cursore a prima di esso</param>
+        /// <param name="righe">Numero di righe da cancellare</param>
+        /// <param name="left">Posizione sinistra del cursore da ripristinare</param>
+        public static void MessaggioSelezioneErrata(int ms = 600, bool resetCursore = false, int righe = 0, int left = 0)
+        {
+            var cursore = Console.GetCursorPosition();
             Console.Write("\n...selezione errata"); //TO DO spectre
             Thread.Sleep(600);
+            if (resetCursore)
+            {
+                Console.SetCursorPosition(left, cursore.Top - 1);
+                for (int i = 0; i < righe; i++)
+                {
+                    Console.WriteLine("                                                              ");
+                }
+                Console.SetCursorPosition(left, cursore.Top - 1);
+            }
+        }
+
+        /// <summary>
+        /// Pulisce la console e ripristina il cursore a prima del messaggio
+        /// </summary>
+        /// <param name="left">Posizione left del cursore prima del messaggio</param>
+        /// <param name="top">Posizione top del cursore prima del messaggio</param>
+        public static void PulisciRiga(int left, int top)
+        {
+            Console.SetCursorPosition(left, top);
+            for (int i = 0; i < top; i++)
+            {
+                Console.WriteLine("                                     ");
+            }
+            Console.SetCursorPosition(left, top);
         }
     }
 }
