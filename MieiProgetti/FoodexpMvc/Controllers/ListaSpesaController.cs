1ebd1ffd61410a1d3200a6c25731b78fac174674
Simone <simone.ussi85@gmail.com>
2024-03-12 19:41:15 +0100

    Prima versione metodo aggiungi lista spesa

--- MieiProgetti/FoodexpMvc/Controllers/ListaSpesaController.cs
+++ MieiProgetti/FoodexpMvc/Controllers/ListaSpesaController.cs
@@ -9,6 +9,9 @@ namespace FoodexpMvc.Controllers
         private static bool eseguito;
 
         #region Switch Selezione
+        /// <summary>
+        /// Gestisce la selezione del sotto-menu "Gestione lista della spesa"
+        /// </summary>
         public static void SelezioneMenu()
         {
             //gestione lista della spesa
@@ -49,7 +52,6 @@ namespace FoodexpMvc.Controllers
                     case "r":
                         //torna al menu principale
                         View.MessaggioTornaMenuPrincipale();
-                        eseguito = true;
                         break;
 
                     default:
@@ -62,9 +64,110 @@ namespace FoodexpMvc.Controllers
         #endregion
 
         #region C - Aggiungi alimento lista
+        /// <summary>
+        /// Aggiunge un oggetto di tipo ListaSpesa al db.ListaSpesa
+        /// </summary>
         public static void AggiungiInListaSpesa()
         {
+            string input;
 
+            eseguito = false;
+            while (!eseguito)
+            {
+                Console.Clear();
+
+                //nome
+                Console.Write("Inserisci alimento: ");
+                var cursore = Console.GetCursorPosition();
+                string alimento = ValidaInput.GetString(cursore.Left, cursore.Top);
+
+                //quantita
+                Console.Write("Inserisci quantita: ");
+                cursore = Console.GetCursorPosition();
+                int quantita = ValidaInput.GetQuantita(cursore.Left, cursore.Top);
+
+                //verifica se gia presente
+                var r = _db.ListaSpesa.FirstOrDefault(l => l.Alimento == alimento);
+
+                if (r != null)  //gia presente chiedo cosa fare
+                {
+                    Console.WriteLine("Alimento gia presente nella lista\n");
+                    Console.WriteLine($"{r.Alimento} - {r.Quantita} - {_db.Categorie.First(c => c.Id == r.CategoriaId).Nome} - {_db.Utenti.First(u => u.Id == r.UtenteId).Nome}");
+
+                    Console.Write("\nVuoi modificare la quantità? (s/n) ");
+                    cursore = Console.GetCursorPosition();
+                    input = ValidaInput.GetSiNo(cursore.Left, cursore.Top);
+
+                    if (input == "s")   //modifico la quantita
+                    {
+                        //quantita
+                        Console.Write("Inserisci quantita: ");
+                        cursore = Console.GetCursorPosition();
+                        quantita = ValidaInput.GetQuantita(cursore.Left, cursore.Top);
+
+                        r.Quantita = quantita;
+                        _db.SaveChanges();
+
+                        //richiesta altro inserimento
+                        Console.Write("\nVuoi aggiungere altro? (s/n)");
+                        cursore = Console.GetCursorPosition();
+                        input = ValidaInput.GetSiNo(cursore.Left, cursore.Top);
+                        if (input == "n")
+                        {
+                            eseguito = true;
+                        }
+                    }
+
+                }
+                else    //alimento non presente quindi ne creo uno nuovo
+                {
+                    //categoria
+                    Console.Clear();
+                    //stampo elenco per l'utente
+                    CategorieView.VisualizzaCategorie(CategorieController.GetCategorie());
+                    int totaleCategorie = _db.Categorie.Count();
+                    Console.Write("\nSeleziona categoria: ");
+                    cursore = Console.GetCursorPosition();
+                    int idCategoria = ValidaInput.GetIntElenco(totaleCategorie, cursore.Left, cursore.Top);
+
+                    Console.Clear();
+
+                    Console.WriteLine("Riepilogo inserimento\n");
+                    Console.WriteLine($"Alimento: {alimento}\nQuantità: {quantita}\nCategoria: {_db.Categorie.FirstOrDefault(c => c.Id == idCategoria)!.Nome}\n");
+
+                    //creo nuovo oggetto Alimento
+                    ListaSpesa l = new ListaSpesa();
+
+                    //assegno i valori per ogni attributo
+                    l.Alimento = alimento;
+                    l.Quantita = quantita;
+                    l.CategoriaId = idCategoria;
+                    l.UtenteId = UtenteAttuale!.Id;
+
+                    //conferma
+                    Console.WriteLine("\nConfermi inserimento? (s/n)");
+                    cursore = Console.GetCursorPosition();
+                    input = ValidaInput.GetSiNo(cursore.Left, cursore.Top);
+
+                    if (input == "s")   //confermo inserimento
+                    {
+                        //inserisco nel db e salvo
+                        _db.ListaSpesa.Add(l);
+                        _db.SaveChanges();
+                        Console.WriteLine("Alimento aggiunto alla lista");
+                        Thread.Sleep(800);
+
+                        //richiesta altro inserimento
+                        Console.Write("\nVuoi aggiungere altro? (s/n)");
+                        cursore = Console.GetCursorPosition();
+                        input = ValidaInput.GetSiNo(cursore.Left, cursore.Top);
+                        if (input == "n")
+                        {
+                            eseguito = true;
+                        }
+                    }
+                }
+            }
         }
 
         /// <summary>
