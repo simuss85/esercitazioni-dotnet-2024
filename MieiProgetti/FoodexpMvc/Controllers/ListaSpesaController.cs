aa849733ec5e736e9952ea63b08cfe83250df8e7
simuss85 <simone.ussi85@gmail.com>
2024-03-13 09:12:55 +0100

    Completati tutti i metodi CRUD

--- MieiProgetti/FoodexpMvc/Controllers/ListaSpesaController.cs
+++ MieiProgetti/FoodexpMvc/Controllers/ListaSpesaController.cs
@@ -211,21 +211,176 @@ namespace FoodexpMvc.Controllers
         #endregion
 
         #region U - Modifica alimento lista
+        /// <summary>
+        /// Permette di modificare un record da db.ListaSpesa selezionando da un elenco numerato.
+        /// </summary>
         public static void ModificaListaSpesa()
         {
+            eseguito = false;
+            while (!eseguito)    //inizio la procedura di modifica
+            {
+                Console.Clear();
 
+                ListaSpesaView.VisualizzaLista(GetLista());
+                var lista = _db.ListaSpesa.ToList();   //per la corretta corrispondenza tra elenco e id
+                int totLista = lista.Count;
+                Console.WriteLine($"r - Torna al menu precedente");  //inserisco opzione di uscita dinamica
+                Console.WriteLine("\nCosa vuoi modificare? ");
+                var cursore = Console.GetCursorPosition();
+                int id = ValidaInput.GetIntElenco(totLista, cursore.Left, cursore.Top, true);
+
+                if (id == -1)   //premuto r - torna al menu precedente
+                {
+                    eseguito = true;
+                    View.MessaggioTornaMenuPrecedente(800);
+                }
+                else    //ho selezionato un alimento dalla lista 
+                {
+                    eseguito = false;
+                    id--;       //index parte da 0
+
+                    //carico alimento da modificare
+                    var l = lista[id];
+
+                    //preparo le variabili di appoggio per l'inserimento dati dell'utente
+                    string? alimento = l.Alimento;
+                    int quantita = l.Quantita;
+                    int idCategoria = l.CategoriaId;
+                    int idUtente = l.UtenteId;
+                    string input;
+
+                    Console.Clear();
+
+                    Console.WriteLine("Riepilogo alimento\n");
+                    Console.WriteLine($"1 - Alimento: {alimento}\n2 - Quantità: {quantita}\n3 - Categoria: {_db.Categorie.First(c => c.Id == l.Id).Nome}\nr - Tutto corretto");
+                    //richiesta di modifica
+                    Console.Write("\nSeleziona l'elemento da modificare: ");
+                    cursore = Console.GetCursorPosition();
+                    int idSelezione = ValidaInput.GetIntElenco(3, cursore.Left, cursore.Top, true);
+
+                    switch (idSelezione)
+                    {
+                        case 1:
+                            //nome
+                            Console.Write("Inserisci alimento: ");
+                            cursore = Console.GetCursorPosition();
+                            alimento = ValidaInput.GetString(cursore.Left, cursore.Top);
+                            break;
+
+                        case 2:
+                            //quantita
+                            Console.Write("Inserisci quantita: ");
+                            cursore = Console.GetCursorPosition();
+                            quantita = ValidaInput.GetQuantita(cursore.Left, cursore.Top);
+                            break;
+
+                        case 3:
+                            //categoria
+                            Console.Clear();
+                            //stampo elenco per l'utente
+                            CategorieView.VisualizzaCategorie(CategorieController.GetCategorie());
+                            int totaleCategorie = _db.Categorie.Count();
+                            Console.Write("\nSeleziona categoria: ");
+                            cursore = Console.GetCursorPosition();
+                            idCategoria = ValidaInput.GetIntElenco(totaleCategorie, cursore.Left, cursore.Top);
+                            break;
+
+                        case -1:
+                            //tutto corretto non fa niente
+                            eseguito = true;
+                            View.MessaggioTornaMenuPrecedente();
+                            Console.ReadKey();
+                            return;
+
+                        default:
+                            Console.WriteLine("Selezione errata");
+                            break;
+                    }
+                    Console.Clear();
+                    Console.WriteLine("Riepilogo inserimento\n");
+                    Console.WriteLine($"1 - Alimento: {alimento}\n2 - Quantità: {quantita}\n3 - Categoria: {_db.Categorie.First(c => c.Id == l.Id).Nome}\nr - Tutto corretto");
+
+                    //conferma iserimento
+                    Console.WriteLine("\nConfermi inserimento? (s/n)");
+                    cursore = Console.GetCursorPosition();
+                    input = ValidaInput.GetSiNo(cursore.Left, cursore.Top);
+
+                    if (input == "s")
+                    {
+                        //assegno i valori per ogni attributo
+                        l.Alimento = alimento;
+                        l.Quantita = quantita;
+                        l.CategoriaId = idCategoria;
+                        l.UtenteId = UtenteAttuale!.Id;
+                        _db.SaveChanges();
+                    }
+                }
+            }
         }
         #endregion
 
         #region D- Elimina alimento lista
+        /// <summary>
+        /// Permette di eliminare un record di db.ListaSpesa da un elenco numerato fino a quando l'utente seleziona <br/>
+        /// il carattere 0.
+        /// </summary>
         private static void EliminaDaListaSpesa()
         {
+            eseguito = false;
+            while (!eseguito)
+            {
+                Console.Clear();
 
+                ListaSpesaView.VisualizzaLista(GetLista()); //stampa elenco
+                var lista = _db.ListaSpesa.ToList();        //per la corretta corrispondenza tra elenco e id
+                int totLista = lista.Count;
+                Console.WriteLine($"r - Torna al menu precedente");  //inserisco opzione di uscita dinamica
+                Console.WriteLine("\nCosa vuoi eliminare? ");
+                var cursore = Console.GetCursorPosition();
+                int id = ValidaInput.GetIntElenco(totLista, cursore.Left, cursore.Top, true);
+
+                if (id == -1)   //premuto r - torna al menu precedente
+                {
+                    eseguito = true;
+                    View.MessaggioTornaMenuPrecedente(800);
+                }
+                else    //ho selezionato un alimento dalla lista 
+                {
+                    eseguito = false;
+                    id--;       //index parte da 0
+
+                    Console.WriteLine($"Vuoi eliminare {lista[id].Alimento}? (s/n)");
+                    cursore = Console.GetCursorPosition();
+                    string input = ValidaInput.GetSiNo(cursore.Left, cursore.Top);
+
+                    if (input == "s")
+                    {
+                        //seleziono l'alimento in posizione [id] dalla lista di tipo Alimento
+                        //seleziono poi il suo id che passo al metodo che elimina l'oggetto dal db.Alimento
+                        EliminaDaListaPerId(lista[id].Id);
+                        Console.WriteLine("Alimento cancellato dalla lista");
+                    }
+                }
+            }
         }
         #endregion
 
         #region Metodi accessori
-        //eventuali metodi accessori
+        /// <summary>
+        /// Metodo accessorio che elimina un record da db.ListaSpesa tramite il suo Id
+        /// </summary>
+        /// <param name="id">L'id del record da eliminare</param>
+        private static void EliminaDaListaPerId(int id)
+        {
+            foreach (var l in _db.ListaSpesa)
+            {
+                if (l.Id == id)
+                {
+                    _db.ListaSpesa.Remove(l);
+                    _db.SaveChanges();
+                }
+            }
+        }
         #endregion
     }
 }
