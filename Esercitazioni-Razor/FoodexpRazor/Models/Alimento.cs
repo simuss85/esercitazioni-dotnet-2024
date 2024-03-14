dafa87875aff53e458391bff2d2cce353582bc4e
Simone <simone.ussi85@gmail.com>
2024-03-14 19:41:50 +0100

    Creata app razor FoodexpRazor

---
+++ Esercitazioni-Razor/FoodexpRazor/Models/Alimento.cs
@@ -0,0 +1,15 @@
+namespace FoodexpRazor.Models;
+
+public class Alimento
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public int Quantita { get; set; }
+    public DateTime DataScadenza { get; set; }
+    public DateTime DataInserimento { get; set; }
+    //chiave esterna
+    public int CategoriaId { get; set; }
+    public Categoria? Categoria { get; set; } //riferimento molti a uno tab-categoria
+    public string? Info { get; set; }
+
+}
---
+++ Esercitazioni-Razor/FoodexpRazor/Models/Categoria.cs
@@ -0,0 +1,7 @@
+namespace FoodexpRazor.Models;
+
+public class Categoria
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+}
