165f9dfec184a388633aa244c79e898ce11a5175
simuss85 <simone.ussi85@gmail.com>
2024-03-15 15:06:19 +0100

    Aggiunge il passaggio di parametri per dizionario

--- Esercitazioni-Razor/WebAppProdotti/Pages/Prodotti.cshtml.cs
+++ Esercitazioni-Razor/WebAppProdotti/Pages/Prodotti.cshtml.cs
@@ -1,12 +1,16 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebAppProdotti.Models;
+using Newtonsoft.Json;
 
 namespace WebAppProdotti.Pages;
 
 public class ProdottiModel : PageModel
 {
     public required int numeroPagine { get; set; }
+    public required string jsonPath = "wwwroot/json/prodotti.json";
+    public required IEnumerable<Prodotto> Prodotti { get; set; }
+
+
     private readonly ILogger<ProdottiModel> _logger;
 
     public ProdottiModel(ILogger<ProdottiModel> logger)
@@ -14,43 +18,16 @@ public class ProdottiModel : PageModel
         _logger = logger;
     }
 
-    public required IEnumerable<Prodotto> Prodotti { get; set; } //creo oggetto di tipo lista
+
     public void OnGet(decimal? minPrezzo, decimal? maxPrezzo, int? pageIndex)
     {
         //aggiungi un log
         _logger.LogInformation("Tabella prodotti");
-        Prodotti = new List<Prodotto>
-        {
-            new Prodotto {Nome = "Prodotto 1", Prezzo = 100, Dettaglio = "Descrizione prodotto 1"},
-            new Prodotto {Nome = "Prodotto 2", Prezzo = 200, Dettaglio = "Descrizione prodotto 2"},
-            new Prodotto {Nome = "Prodotto 3", Prezzo = 300, Dettaglio = "Descrizione prodotto 3"},
-            new Prodotto {Nome = "Prodotto 4", Prezzo = 400, Dettaglio = "Descrizione prodotto 4"},
-            new Prodotto {Nome = "Prodotto 5", Prezzo = 500, Dettaglio = "Descrizione prodotto 5"},
-            new Prodotto {Nome = "Prodotto 6", Prezzo = 600, Dettaglio = "Descrizione prodotto 6"},
-            new Prodotto {Nome = "Prodotto 7", Prezzo = 700, Dettaglio = "Descrizione prodotto 7"},
-            new Prodotto {Nome = "Prodotto 8", Prezzo = 800, Dettaglio = "Descrizione prodotto 8"},
-            new Prodotto {Nome = "Prodotto 9", Prezzo = 900, Dettaglio = "Descrizione prodotto 9"},
-            new Prodotto {Nome = "Prodotto 10", Prezzo = 100, Dettaglio = "Descrizione prodotto 10"},
-            new Prodotto {Nome = "Prodotto 11", Prezzo = 200, Dettaglio = "Descrizione prodotto 11"},
-            new Prodotto {Nome = "Prodotto 12", Prezzo = 300, Dettaglio = "Descrizione prodotto 12"},
-            new Prodotto {Nome = "Prodotto 13", Prezzo = 300, Dettaglio = "Descrizione prodotto 13"},
-            new Prodotto {Nome = "Prodotto 14", Prezzo = 400, Dettaglio = "Descrizione prodotto 14"},
-            new Prodotto {Nome = "Prodotto 15", Prezzo = 500, Dettaglio = "Descrizione prodotto 15"},
-            new Prodotto {Nome = "Prodotto 16", Prezzo = 600, Dettaglio = "Descrizione prodotto 16"},
-            new Prodotto {Nome = "Prodotto 17", Prezzo = 100, Dettaglio = "Descrizione prodotto 17"},
-            new Prodotto {Nome = "Prodotto 18", Prezzo = 200, Dettaglio = "Descrizione prodotto 18"},
-            new Prodotto {Nome = "Prodotto 19", Prezzo = 300, Dettaglio = "Descrizione prodotto 19"},
-            new Prodotto {Nome = "Prodotto 20", Prezzo = 300, Dettaglio = "Descrizione prodotto 20"},
-            new Prodotto {Nome = "Prodotto 21", Prezzo = 400, Dettaglio = "Descrizione prodotto 21"},
-            new Prodotto {Nome = "Prodotto 22", Prezzo = 500, Dettaglio = "Descrizione prodotto 22"},
-            new Prodotto {Nome = "Prodotto 23", Prezzo = 600, Dettaglio = "Descrizione prodotto 23"},
-            new Prodotto {Nome = "Prodotto 24", Prezzo = 100, Dettaglio = "Descrizione prodotto 24"},
-            new Prodotto {Nome = "Prodotto 25", Prezzo = 200, Dettaglio = "Descrizione prodotto 25"},
-            new Prodotto {Nome = "Prodotto 26", Prezzo = 300, Dettaglio = "Descrizione prodotto 26"},
-            new Prodotto {Nome = "Prodotto 27", Prezzo = 300, Dettaglio = "Descrizione prodotto 27"},
-            new Prodotto {Nome = "Prodotto 28", Prezzo = 400, Dettaglio = "Descrizione prodotto 28"},
-            new Prodotto {Nome = "Prodotto 29", Prezzo = 500, Dettaglio = "Descrizione prodotto 29"},
-        };
+
+        string jsonFile = System.IO.File.ReadAllText(jsonPath);
+
+        Prodotti = JsonConvert.DeserializeObject<List<Prodotto>>(jsonFile)!;
+
         if (minPrezzo.HasValue)
         {
             Prodotti = Prodotti.Where(p => p.Prezzo >= minPrezzo);
--- Esercitazioni-Razor/WebAppProdotti/Pages/ProdottoDettaglio.cshtml.cs
+++ Esercitazioni-Razor/WebAppProdotti/Pages/ProdottoDettaglio.cshtml.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebAppProdotti.Models;
 
@@ -6,18 +5,23 @@ namespace WebAppProdotti.Pages;
 
 public class ProdottoDettaglioModel : PageModel
 {
-    private readonly ILogger<ProdottiModel> _logger;
+    public required Prodotto Prodotto { get; set; }
 
-    public ProdottoDettaglioModel(ILogger<ProdottiModel> logger)
+
+    private readonly ILogger<ProdottoDettaglioModel> _logger;
+
+    public ProdottoDettaglioModel(ILogger<ProdottoDettaglioModel> logger)
     {
         _logger = logger;
     }
-    public required Prodotto Prodotto { get; set; }
-    public void OnGet(string nome, string dettaglio, decimal prezzo)
+
+    public void OnGet(Prodotto prodotto)
     {
-        Prodotto = new Prodotto { Nome = nome, Prezzo = prezzo, Dettaglio = dettaglio };
-        //aggiungi un log
-        _logger.LogInformation("Dettaglio prodotti");
+        Prodotto = prodotto;
+        _logger.LogInformation($"***argomento***nome: {prodotto.Nome}, prezzo: {prodotto.Prezzo}, dettaglio: {prodotto.Dettaglio}");
+
+        // _logger.LogInformation($"***assegnamento***nome: {Prodotto.Nome}, prezzo: {Prodotto.Prezzo}, dettaglio: {Prodotto.Dettaglio}");
+
     }
 }
 
