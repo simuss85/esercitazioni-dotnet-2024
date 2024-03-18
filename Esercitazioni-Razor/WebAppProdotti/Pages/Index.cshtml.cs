e7c2d8391973e34358654026608648f899b860d4
simuss85 <simone.ussi85@gmail.com>
2024-03-18 15:39:21 +0100

    Fixa errori vari

--- Esercitazioni-Razor/WebAppProdotti/Pages/Index.cshtml.cs
+++ Esercitazioni-Razor/WebAppProdotti/Pages/Index.cshtml.cs
@@ -14,6 +14,7 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
+
     }
 
 }
--- Esercitazioni-Razor/WebAppProdotti/Pages/ProdottoDettaglio.cshtml.cs
+++ Esercitazioni-Razor/WebAppProdotti/Pages/ProdottoDettaglio.cshtml.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 using WebAppProdotti.Models;
 
 namespace WebAppProdotti.Pages;
@@ -6,7 +7,8 @@ namespace WebAppProdotti.Pages;
 public class ProdottoDettaglioModel : PageModel
 {
     public required Prodotto Prodotto { get; set; }
-
+    public required string jsonPath = "wwwroot/json/prodotti.json";
+    public required IEnumerable<Prodotto> Prodotti { get; set; }
 
     private readonly ILogger<ProdottoDettaglioModel> _logger;
 
@@ -22,6 +24,22 @@ public class ProdottoDettaglioModel : PageModel
 
         // _logger.LogInformation($"***assegnamento***nome: {Prodotto.Nome}, prezzo: {Prodotto.Prezzo}, dettaglio: {Prodotto.Dettaglio}");
 
+    }
+
+    public void OnPost(Prodotto prodotto)
+    {
+        _logger.LogInformation("prodotto: {0} prezzo nuovo: {1} ", prodotto.Nome, prodotto.Prezzo);
+        string jsonFile = System.IO.File.ReadAllText(jsonPath);
+        Prodotti = JsonConvert.DeserializeObject<List<Prodotto>>(jsonFile)!;
+        foreach (var p in Prodotti)
+        {
+            if (p.Nome == prodotto.Nome)
+            {
+                p.Prezzo = prodotto.Prezzo;
+            }
+        }
+
+        System.IO.File.WriteAllText(jsonPath, JsonConvert.SerializeObject(Prodotti));
     }
 }
 
