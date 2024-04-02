858ea63fa73f76ef91650e73e80183922f0dc4c9
simuss85 <simone.ussi85@gmail.com>
2024-04-02 12:49:35 +0200

    Primo commit prova Mvc IdentityF

---
+++ MieiProgetti/FotoGalleryMvcId/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FotoGalleryRazorId.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AccountController(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    // gestione del ruolo Amministratore/modertatore
+    public async Task<IActionResult> AddToRoleAdmin()
+    {
+        var user = await _userManager.FindByNameAsync(User.Identity!.Name!);
+        await _userManager.AddToRoleAsync(user!, "Admin");
+        return RedirectToAction("Index", "Pages");
+    }
+
+    //gestione del ruolo User/utente registrato
+    public async Task<IActionResult> AddToRoleUser()
+    {
+        var user = await _userManager.FindByNameAsync(User.Identity!.Name!);
+        await _userManager.AddToRoleAsync(user!, "User");
+        return RedirectToAction("Index", "Pages");
+    }
+}
