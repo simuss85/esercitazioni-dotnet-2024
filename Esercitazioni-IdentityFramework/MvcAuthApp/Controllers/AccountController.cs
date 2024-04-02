df745f1917a25f25fedcaf4b7fcba9d2a2af526e
simuss85 <simone.ussi85@gmail.com>
2024-04-02 09:39:24 +0200

    Aggiunge gestione Admin e User

--- Esercitazioni-IdentityFramework/MvcAuthApp/Controllers/AccountController.cs
+++ Esercitazioni-IdentityFramework/MvcAuthApp/Controllers/AccountController.cs
@@ -12,11 +12,19 @@ public class AccountController : Controller
         _userManager = userManager;
     }
 
-    // GET: /Account/AddToRole
-    public async Task<IActionResult> AddToRole()
+    // GET: /Account/AddToRoleAdmin
+    public async Task<IActionResult> AddToRoleAdmin()
     {
         var user = await _userManager.FindByNameAsync(User.Identity!.Name!);
         await _userManager.AddToRoleAsync(user!, "Admin");
+        return RedirectToAction("Index", "Home");
+    }
+
+    //GET: /Account/AddToRoleUser
+    public async Task<IActionResult> AddToRoleUser()
+    {
+        var user = await _userManager.FindByNameAsync(User.Identity!.Name!);
+        await _userManager.AddToRoleAsync(user!, "User");
         return RedirectToAction("Index", "Home");
     }
 
@@ -27,4 +35,21 @@ public class AccountController : Controller
         var roles = await _userManager.GetRolesAsync(user!);
         return Content(string.Join(", ", roles));
     }
+
+    //GET: /Account/RemoveFromRoleAdmin
+    public async Task<IActionResult> RemoveFromRoleAdmin()
+    {
+        var user = await _userManager.FindByNameAsync(User.Identity!.Name!);
+        await _userManager.RemoveFromRoleAsync(user!, "Admin");
+        return RedirectToAction("Index", "Home");
+    }
+
+    //GET: /Account/RemoveFromRoleUser
+    public async Task<IActionResult> RemoveFromRoleUser()
+    {
+        var user = await _userManager.FindByNameAsync(User.Identity!.Name!);
+        await _userManager.RemoveFromRoleAsync(user!, "User");
+        return RedirectToAction("Index", "Home");
+    }
+
 }
