1a29f3a1dd129226127536b65d5b1d5abf962830
simuss85 <simone.ussi85@gmail.com>
2024-04-04 14:14:49 +0200

    Aggiunge il controllo url

--- MieiProgetti/FotoGalleryRazorId/Models/InputModel.cs
+++ MieiProgetti/FotoGalleryRazorId/Models/InputModel.cs
@@ -16,7 +16,12 @@ public class InputModel
     [Display(Name = "Categoria")]
     public string? Categoria { get; set; }
 
+
     [Required(ErrorMessage = "Devi inserire un link")]
     [Display(Name = "Link immagine")]
+    [Url(ErrorMessage = "Url non valido")]
+    [RegularExpression(@".+\.(jpg|jpeg)$", ErrorMessage = "L'URL deve contenere un'imagine .jpg o .jpeg")]
     public string? Path { get; set; }
+
+
 }
