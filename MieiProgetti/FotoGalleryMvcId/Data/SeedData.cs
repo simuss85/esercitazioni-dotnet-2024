ac52e58c8e8077783d7b72ab2c19686efa431e60
simuss85 <simone.ussi85@gmail.com>
2024-04-08 11:00:30 +0200

    Inizializza ed esegue lo scaffolding di Register

---
+++ MieiProgetti/FotoGalleryMvcId/Data/SeedData.cs
@@ -0,0 +1,37 @@
+using FotoGalleryMvcId.Models;
+using Microsoft.AspNetCore.Identity;
+
+//!!! classe che crea i ruoli nel dbContext 
+public class SeedData
+{
+    public static async Task InitializeAsync(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager)
+    {
+        //creazione dei ruoli se non esistono
+        string[] roleNames = { "Admin", "Moderatore", "User" };
+
+        foreach (var roleName in roleNames)
+        {
+            if (!await roleManager.RoleExistsAsync(roleName))
+            {
+                await roleManager.CreateAsync(new IdentityRole(roleName));
+            }
+        }
+
+        //creazione dell'utente Admin se non esiste (personalizzare con i mio)
+        if (await userManager.FindByEmailAsync("admin@admin.com") == null)
+        {
+            var adminUser = new AppUser
+            {
+                UserName = "admin@admin.com",
+                Email = "admin@amin.com",
+                Nome = "Admin",
+                Cognome = "Admin",
+                Eta = 18,
+                Alias = "Admin",
+                EmailConfirmed = true
+            };
+            await userManager.CreateAsync(adminUser, "Admin123!");
+            await userManager.AddToRoleAsync(adminUser, "Admin");
+        }
+    }
+}
--- MieiProgetti/FotoGalleryMvcId/Program.cs
+++ MieiProgetti/FotoGalleryMvcId/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using FotoGalleryMvcId.Data;
+using FotoGalleryMvcId.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,11 +11,34 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+
+builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)  //!!! Modifica da IdentityUser ad AppUser (1/4)
+    .AddRoles<IdentityRole>()   //!!! aggiunto per gestire i ruoli  (2/4)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+// .AddDefaultTokenProviders();    //!!! per aggiungere i campi nuovi in Register (DA TESTARE)
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+//!!! seeding del database    (3/4)
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        //Risolvi il RoleManager del provider di servizi
+        var userManager = serviceProvider.GetRequiredService<UserManager<AppUser>>();  //!!! aggiunto per gestire model AppUser (4/4)
+        var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+        //chiamata al metodo per assicurare che i ruoli esistano
+        await SeedData.InitializeAsync(userManager, roleManager);
+    }
+    catch (Exception ex)
+    {
+        var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Un errore è avvenuto durante il seeding del database");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -41,3 +65,4 @@ app.MapControllerRoute(
 app.MapRazorPages();
 
 app.Run();
+
