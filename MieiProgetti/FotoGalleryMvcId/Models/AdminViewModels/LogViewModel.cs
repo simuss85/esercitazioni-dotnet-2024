ca047e2dbac6f160a4f1881f4016bb86099824c7
Simone <simone.ussi85@gmail.com>
2024-04-16 18:40:31 +0200

    Corretta la paginazione dei log

--- MieiProgetti/FotoGalleryMvcId/Models/AdminViewModels/LogViewModel.cs
+++ MieiProgetti/FotoGalleryMvcId/Models/AdminViewModels/LogViewModel.cs
@@ -20,7 +20,7 @@ public class LogViewModel
     //attibuti per le view GET
     public IEnumerable<Log>? Logs { get; set; }
     public int NumeroPagine { get; set; }
-    public int? PageIndex { get; set; }
+    public int PageIndex { get; set; }
     public int ElementiPerPagina { get; set; }
     public string? Reverse { get; set; }    //gestisce filtri di ordinamento
 
