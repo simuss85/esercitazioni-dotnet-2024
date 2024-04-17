4427840830a978ae1ea24606f7c1d8e2ee733ac9
simuss85 <simone.ussi85@gmail.com>
2024-04-17 09:03:15 +0200

    Aggiunge paginazione dinamica

--- MieiProgetti/FotoGalleryMvcId/Models/ModeratoreViewModels/ModeraCommentiViewModel.cs
+++ MieiProgetti/FotoGalleryMvcId/Models/ModeratoreViewModels/ModeraCommentiViewModel.cs
@@ -19,7 +19,7 @@ public class ModeraCommentiViewModel
 
     //attibuti per le view GET
     public int NumeroPagine { get; set; }
-    public int? PageIndex { get; set; }
+    public int PageIndex { get; set; }
     public int ElementiPerPagina { get; set; }
     public string? Reverse { get; set; }    //gestisce filtri di ordinamento
 
